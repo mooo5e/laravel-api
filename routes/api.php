f027a8a176818bd66c67e2d2c9ecf00af00549b3
mooo5e <mooose@ex.ua>
2021-06-01 22:58:58 +0300

    added first test get

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/test', function(){
+    return ['message' => 'hello!'];
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
